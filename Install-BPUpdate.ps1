e4430180662b1d35599bcbda9bb7c5179b6bb19e
James Smith <psych0ticus.aus@gmail.com>
2017-07-21 12:24:42 +1000

    Complete rewrite

--- Install-BPUpdate.ps1
+++ Install-BPUpdate.ps1
@@ -1,54 +1,105 @@
 #Requires -RunAsAdministrator
 [CmdletBinding()]
-param ( [string]$Date = (get-date -Format yyMMdd),
-        [string]$Destination = "C:\BPUpdates",
-	    [string]$DownloadURL = "http://wpc.c567.edgecastcdn.net/00c567/BPS_Data_"+$Date+"_inc.exe",
-	    [string]$OutFileName = "BPS_Data_"+$Date+"_inc.exe",
-        [string]$EventSource = "Best Practice Update Script"
+param ( [string]$EventSource = 'Best Practice Update Script',
+  [string]$Destination = "$env:HOMEDRIVE\BPUpdates",
+  [string]$CurrentMonth = (Get-Date -Format MMMM),
+  [string]$OutFileName = 'BPS_Data_'+$CurrentMonth+'_inc.exe'
 )
+
 
 ###############
 #    NOTICE   #   This MUST be run on your Best Practice server.
 ###############
 
-# Check to see if the event source esists and if not create it.
-# https://stackoverflow.com/questions/13851577/how-to-determine-if-an-eventlog-already-exists
-$EventSourceExists = Get-EventLog -list | Where-Object {$_.logdisplayname -eq "$EventSource"} 
-IF (!($EventSourceExists)) 
-    {
-        New-EventLog -LogName Application -Source $EventSource
-    }
 
+
+# Check to see if the event source esists and if not create it.
+$EventSourceExists = Get-EventLog  -LogName Application | Where-Object -FilterScript {
+  $_.source -like "$EventSource"
+} 
+
+IF ($EventSourceExists -eq $Null) 
+{
+  New-EventLog -LogName Application -Source $EventSource
+}
 
 # Check to see if the download folder exists and if not create it.
-if(!(Test-Path $Destination -PathType Container -ErrorAction SilentlyContinue))
+IF ((Test-Path -Path $Destination -PathType Container -ErrorAction SilentlyContinue) -eq $False)
+{
+  New-Item -Path $Destination -ItemType Directory
+}
+
+
+# This opens the page to allow us to interact with it via the script.
+$Request  = Invoke-WebRequest -Uri 'http://www.bpsoftware.net/updates/data-update/'
+
+
+# This section looks for the release month on the page. It then compares it to the current month.
+# If the current month and release month are the same it checks to see if it has already applied this patch.
+$ReleaseMonth = $Request.ParsedHTML.getelementsbytagname('p') |
+Where-Object -FilterScript {
+  $_.InnerText -Like '*data update has been released*'
+} |
+Select-Object -ExpandProperty InnerText
+
+
+#Check to see if this month has already been installed.
+$AlreadyInstalled = Get-EventLog -LogName Application | Where-Object {$_.Message -like "Successfully installed the $CurrentMonth data update."}
+
+ IF ($AlreadyInstalled)
     {
-        New-Item $Destination -type Directory
-    }
-
-TRY {
-	    # Download the file to the download folder. If this fails stop the script and write the error to $DownloadError
-	    Invoke-WebRequest $DownloadURL -OutFile $OutFileName -ErrorAction Stop -ErrorVariable DownloadError
-    }
-
-    CATCH {
-	        write-host "No download available" -ForegroundColor Red
-            # Write an event to event viewer showing the download URL tried and the error produced.
-            Write-EventLog -LogName Application -Source $EventSource -EntryType Warning -EventId 100 -Message "There was no download available. The download URL tried was $DownloadURL `r`n`n $DownloadError"
+      IF ($ReleaseMonth -like $CurrentMonth)
+          {
+            Write-EventLog -LogName Application -Source $EventSource -EntryType Information -EventId 100 -Message "$CurrentMonth update has already been installed."
             Exit
           }
-
-TRY {
-        write-host "Starting install..." -ForgroundColor Green
-	    # Start the update installer silently. If it fails write the error to $InstallError.
-	    Start-Process -FilePath $Destination\$OutFileName -ArgumentList ('/s') -ErrorAction Stop -ErrorVariable InstallError
-    }
-
-    CATCH {
-            write-host "Install failed" -ForegroundColor Red
-            # Write an event to event viewer advising the install failed and insert the error message produced.
-            Write-EventLog -LogName Application -Source $EventSource -EventId 101 -EntryType Error -Message "The installation of the update has failed. `r`n`n $InstallError"
-            Exit
-          }
-          
         
+    }
+
+
+# This sections finds the download links present on the data update page and then adds them to the $EXELinks variable.
+$ExeLinks = $Request.ParsedHtml.getElementsByTagName('input') |
+Where-Object -FilterScript {
+  $_.Type -eq 'hidden' -and $_.Value -like 'http*exe' 
+} |
+Select-Object -ExpandProperty Value
+$CurrentDownload = $ExeLinks[0]
+
+
+IF ( $ReleaseMonth -Like "*$CurrentMonth*")
+{
+  # This section writes an event log that it is going to attempt a download.
+  # It then attempts to download the file.
+  # The script then checks if the download was successful and writes an event log entry.
+  TRY 
+  {
+    Write-EventLog -LogName Application -Source $EventSource -EntryType Information -EventId 100 -Message "Attempting to download this months update. The URL is $CurrentDownload"
+    $Download = Invoke-WebRequest -Uri $CurrentDownload -OutFile "$Destination\$OutFileName" -ErrorVariable DownloadError -PassThru
+
+    IF ($Download.StatusCode -eq '200') 
+    {
+      Write-EventLog -LogName Application -Source $EventSource -EntryType Information -EventId 102 -Message "The download was successful. The URL is $CurrentDownload"
+      Write-EventLog -LogName Application -Source $EventSource -EntryType Information -EventId 100 -Message "Attempting to install the $CurrentMonth update."
+                            
+      TRY 
+      {
+        # Start the update installer silently. If it fails write the error to $InstallError.
+        Start-Process -FilePath $Destination\$OutFileName -ArgumentList ('/s') -ErrorAction Stop -ErrorVariable InstallError
+        Write-EventLog -LogName Application -Source $EventSource -EntryType Information -EventId 102 -Message "Successfully installed the $CurrentMonth data update."
+      }
+
+      CATCH 
+      {
+        # Write an event to event viewer advising the install failed and insert the error message produced.
+        Write-EventLog -LogName Application -Source $EventSource -EventId 101 -EntryType Error -Message "The installation of the update has failed. `r`n`n $InstallError"
+        Exit
+      }
+    }
+  }
+
+  CATCH 
+  {
+    Write-EventLog -LogName Application -Source $EventSource -EntryType Warning -EventId 101 -Message "Failed to download the file from URL $CurrentDownload `r`n`n $DownloadError"
+    Exit
+  }
+}
